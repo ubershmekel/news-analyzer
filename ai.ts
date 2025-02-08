20df4f3b31ee7384f7176bd720419ed527cc77fe
Yuval Greenfield <ubershmekel@gmail.com>
2025-02-08 00:35:11 -0800

    Deepseek but whoops i have no balance

--- ai.ts
+++ ai.ts
@@ -2,17 +2,24 @@ import OpenAI from "openai";
 
 import secrets from "./secrets.json";
 
+const isDeepSeek = false;
+
 const openai = new OpenAI({
-  apiKey: secrets.OPENAI_API_KEY,
+  // apiKey: secrets.OPENAI_API_KEY,
+  apiKey: isDeepSeek ? secrets.DEEPSEEK_API_KEY : secrets.OPENAI_API_KEY,
+  baseURL: isDeepSeek ? "https://api.deepseek.com" : "https://api.openai.com",
 });
 
+// UnprocessableEntityError: 422 Failed to deserialize the JSON body into the target type: messages[0].role:
+// unknown variant `developer`, expected one of `system`, `user`, `assistant`, `tool` at line 4 column 25
 export async function ask(question: string) {
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "developer", content: "You are a concise news editor." },
+      // { role: "developer", content: "You are a concise news editor." },
+      { role: "system", content: "You are a concise news editor." },
       { role: "user", content: question },
     ],
-    model: "gpt-4o",
+    model: isDeepSeek ? "deepseek-chat" : "gpt-4o",
     store: true,
   });
 
