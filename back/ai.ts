e7757152121c1f72a5872f6cd1337bb96c16d0a4
Yuval Greenfield <ubershmekel@gmail.com>
2025-02-09 10:22:32 -0800

    Support hugging face api

--- back/ai.ts
+++ back/ai.ts
@@ -2,26 +2,42 @@ import OpenAI from "openai";
 
 import secrets from "./secrets.json";
 
-const isDeepSeek = false;
+const apiData = {
+  openai: {
+    baseUrl: "https://api.openai.com/v1",
+    model: "gpt-4o",
+    apiKey: secrets.OPENAI_API_KEY,
+  },
+  huggFace: {
+    baseUrl: "https://huggingface.co/api/inference-proxy/together",
+    model: "deepseek-ai/DeepSeek-V3",
+    apiKey: secrets.HUGGINGFACE_API_KEY,
+  },
+  deepSeek: {
+    baseUrl: "https://api.deepseek.com",
+    model: "deepseek-chat",
+    apiKey: secrets.DEEPSEEK_API_KEY,
+  },
+};
 
-const openai = new OpenAI({
+const activeApi = apiData.huggFace;
+
+const client = new OpenAI({
   // apiKey: secrets.OPENAI_API_KEY,
-  apiKey: isDeepSeek ? secrets.DEEPSEEK_API_KEY : secrets.OPENAI_API_KEY,
-  baseURL: isDeepSeek
-    ? "https://api.deepseek.com"
-    : "https://api.openai.com/v1",
+  apiKey: activeApi.apiKey,
+  baseURL: activeApi.baseUrl,
 });
 
 // UnprocessableEntityError: 422 Failed to deserialize the JSON body into the target type: messages[0].role:
 // unknown variant `developer`, expected one of `system`, `user`, `assistant`, `tool` at line 4 column 25
 export async function ask(question: string) {
-  const completion = await openai.chat.completions.create({
+  const completion = await client.chat.completions.create({
     messages: [
       // { role: "developer", content: "You are a concise news editor." },
       { role: "system", content: "You are a concise news editor." },
       { role: "user", content: question },
     ],
-    model: isDeepSeek ? "deepseek-chat" : "gpt-4o",
+    model: activeApi.model,
     store: true,
   });
 
